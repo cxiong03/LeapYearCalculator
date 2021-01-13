77e39c9a9905598162f12d25b7abf725edcdb792
Chang <cnxiong03@gmail.com>
2021-01-13 18:06:14 -0500

    calculate leap year

---
+++ src/com/codewithchang/Main.java
@@ -0,0 +1,10 @@
+package com.codewithchang;
+
+public class Main {
+
+    public static void main(String[] args) {
+        boolean years;
+	    years = LeapYearCalculator.isLeapYear(2400);
+        System.out.println(years);
+    }
+}
